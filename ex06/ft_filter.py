345ac0850291664c7ae69846df331cf8092bc2aa
Hanieh <hanieh@example.com>
2026-04-26 19:35:55 +0400

    Fix ft_filter docstring to match builtin filter.__doc__ exactly

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -2,12 +2,8 @@
 
 
 def ft_filter(function, iterable):
-    """
-    filter(function or None, iterable) --> filter object
-
-    Returns an iterator yielding those items of iterable for which function
-    returns true. If function is None, returns the items that are true.
-    """
+    """Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true."""
     if function is None:
         return iter([item for item in iterable if item])
     return iter([item for item in iterable if function(item)])
